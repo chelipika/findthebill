78b983253388463c6deaf178a2859cdc52dc99ca
chelipika <pirmatovj15@gmail.com>
2025-12-04 10:50:26 -0700

    country region and state region lists added in handlers.py

--- logic/handlers.py
+++ logic/handlers.py
@@ -5,6 +5,7 @@ import database.requests as rq
 
 
 import requests
+import json
 from datetime import datetime, timedelta
 from collections import defaultdict
 
@@ -43,6 +44,8 @@ urlElectrocity = "/api/v1/electricity/AccountRefresh"
 urlWater = "/api/v1/cold_water/AccountRefresh"
 urlProfile = "/api/v1/accounts/Profile"
 urlHomeList = "/api/v1/home/HomeList"
+urlRegion = "/api/v1/common/DistrictList?region="
+urlCountry = "/api/v1/common/RegionList?uz"
 newURL = url+"/api/v1/regions"
 
 
@@ -70,11 +73,42 @@ class GroupMsg(StatesGroup):
 
 class Gen(StatesGroup):
     wait = State()
-    
+
+
+async def get_state_info(data: list):
+    return data["id"], data["name"], data["natural_gas_id"], data["electricity_id"]
+
 async def get_homes_content(tgId):
     home_names = await rq.get_all_home_names()
     reply_markup = kb.create_home_markup_kb(home_names, tgId)
     return reply_markup, home_names
+async def get_bill_electrocity(electroId):
+    response = requests.get(url=f"{url}{urlHomeList}", headers=headers)
+    return str(response.text)
+async def get_country_regions():
+    response = requests.get(url=f"{url}{urlCountry}",headers=headers)
+    data = response.json()
+
+    #DEBUGING AND TESTING
+    # with open("test.json", "w") as file:
+    #     json.dump(data, file, indent=4)
+    # for region in data:
+    #     print(region["name"])
+
+    return data
+
+async def get_state_regions(state_id):
+    response = requests.get(url=f"{url}{urlRegion}{state_id}&uz", headers=headers)
+    data = response.json()
+
+    # DEBUGING AND TESTING
+    # with open("test.json", "w") as file:
+    #     json.dump(data, file, indent=4)
+    # for region in data:
+    #     print(region["name"])
+
+    return data
+
 
 
 router = Router()
@@ -92,6 +126,7 @@ async def forward_channel_post(message: Message):
 
 @router.message(CommandStart())
 async def start(message: Message, state: FSMContext):
+    await message.answer(await get_bill_electrocity(12))
     user_id = message.from_user.id
     await rq.set_user(tg_id=user_id)
     await state.set_state(Ids.homeName)
@@ -121,17 +156,42 @@ async def profile(message: Message):
     home_name = await rq.get_home_name(tg_id=message.from_user.id)
     await message.answer(f"Sizning uyingiz nomi: {await rq.get_home_name(tg_id=message.from_user.id)}\nSizning elektr energiya hisoblagich ID si: {await rq.get_electricity_id(home_name=home_name)}")
 
+@router.message(Command("region"))
+async def regions_test(message: Message):
+    data = await get_country_regions()
+    reply_markup = kb.create_regions_markup_kb(data,"main")
+    await message.answer("Chose your region",reply_markup=reply_markup)
+
+
+
 @router.message(Command("homes"))
 async def homes(message: Message):
-    reply_markup, home_names = get_homes_content(message.from_user.id)
+    reply_markup, home_names = await get_homes_content(message.from_user.id)
     await message.answer("Ro'yxatdagi barcha uylar nomlari:\n" + "\n".join(home_names), reply_markup=reply_markup)
+
+
+
+
+@router.callback_query(F.data.startswith("region_"))
+async def get_state_regions_query(callback :CallbackQuery):
+    parts = callback.data.split("_")
+    await callback.answer(parts[1])
+    data = await get_country_regions()
+    for state in data:
+        if parts[1] == state["name"]:
+            id, name, natural_gas_id, electricity_id = await get_state_info(state)
+            data = await get_state_regions(id)
+            reply_markup = kb.create_regions_markup_kb(data,"countryRegions")
+            await callback.message.edit_text("Viloyatingizni tanglang",reply_markup=reply_markup)
+
+
+
 
 
 @router.callback_query(F.data.startswith("home_"))
 async def unique_home_handler(callback:CallbackQuery):
     await callback.answer()
-    raw_data = callback.data
-    parts = raw_data.split("_")
+    parts = callback.data.split("_")
     user_electricity_id = await rq.get_electricity_id(parts[2])
     await callback.message.edit_text(f"Sizning {parts[2]} Electrick ID raqami: {user_electricity_id}",reply_markup=kb.create_back_button("homes"))
 
@@ -153,7 +213,12 @@ async def user_navigation_query(callback: CallbackQuery):
                 except Exception:
                     # Ignore if content hasn't changed
                     pass
-
+        case "countryRegions":
+            data = await get_country_regions()
+            reply_markup = kb.create_regions_markup_kb(data,"main")
+            await callback.message.edit_text("Viloyatingizni tanglang",reply_markup=reply_markup)
+        case "main":
+            await callback.message.edit_text("Salom")
 
 @router.message(Command("narrator")) #// /narrator 123456, all users will recieve 123456
 async def narrator(message: Message, command: CommandObject):
